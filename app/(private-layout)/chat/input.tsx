7f19df6a67f1bd8af7ed466afb77e8b4d7d57870
jesbarrios <jesbarrios2@gmail.com>
2024-04-14 15:15:01 -0400

    Fixing akward await

--- app/(private-layout)/chat/input.tsx
+++ app/(private-layout)/chat/input.tsx
@@ -19,11 +19,11 @@ export default function ChatInput() {
   const [input, setInput] = useState<string>(""); 
 
   async function handleSubmit() {
-    await setTextInputHeight(50);
+    setTextInputHeight(50);
     const message = input;
     if (!message) return;
 
-    await setInput("");
+    setInput("");
 
     const apiKey = process.env.NEXT_PUBLIC_SECRETKEY!;
     const response = await newChat({ apiKey, message });
