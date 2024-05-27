5bed5a7a5f909e20c5b62b8199157349561c4feb
jesbarrios <jesbarrios2@gmail.com>
2024-05-27 17:30:01 -0400

    new icon and metadffata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import NextAuthProvider from "@/components/session-provider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import Head from "next/head";
 
 const font = Space_Grotesk({ subsets: ["latin"], weight: "400" });
 
@@ -21,6 +22,33 @@ export default function RootLayout({
   return (
     <NextAuthProvider>
       <html lang="en" suppressHydrationWarning>
+        <Head>
+          <title>Scholarly Mind</title>
+          <meta
+            name="description"
+            content="Scholarly Mind serves as a virtual tutor and knowledgeable companion, providing assistance and guidance to empower students in unlocking their full potential."
+          />
+          <link rel="icon" type="image/png" href="/app/icon.png" />
+          <meta property="og:title" content="Scholarly Mind" />
+          <meta
+            property="og:description"
+            content="Scholarly Mind serves as a virtual tutor and knowledgeable companion, providing assistance and guidance to empower students in unlocking their full potential."
+          />
+          <meta property="og:image" content="app/icon.png" />
+          <meta property="og:url" content="https://scholarlymind.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta
+            name="keywords"
+            content="Scholarly Mind, scholarly mind, lifting aspirations to new heights, lift aspirations to new heights, education, academic unpreparedness, AI, ai tutor, AI TUTOR"
+          />
+          <meta name="robots" content="index, follow" />
+
+          <link rel="nav" href="https://www.scholarlymind.com/register" />
+          <link rel="nav" href="https://www.scholarlymind.com/login" />
+          <link rel="nav" href="https://www.scholarlymind.com/" />
+          <link rel="nav" href="https://www.scholarlymind.com/chat" />
+        </Head>
         <body className={`${font.className}`} suppressHydrationWarning>
           <ThemeProvider
             attribute="class"
